a90be985114f12571a7d94cb9108db26cbb40f1a
Byron Lutz <byronlutz@gmail.com>
2013-10-14 13:14:29 -0700

    Fancy pull quotes styled

--- js/app.js
+++ js/app.js
@@ -15,4 +15,12 @@ $(document).ready(function(){
     }, 500);
     $(this).data('rolloverTimeout', rolloverTimeout);
   });
+  
+  // Make pull quotes parallax-ey
+  $(window).scroll(function(){
+    $('.pull-quote.parallax').each(function(){
+      var offset = window.scrollY - $(this).offset().top;
+      $(this).css('background-position-y', offset);
+    });
+  });
 });
