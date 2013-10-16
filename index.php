db00f51b8bfffa877e4dd7c3db9ba31c18309bb5
Byron Lutz <byronlutz@gmail.com>
2013-10-16 03:42:50 -0700

    Added 404 handler to config, refactored things, and added a setting to time the parts

--- index.php
+++ index.php
@@ -4,21 +4,20 @@ $stories = array(
   "DEFAULT" => "dayone",
   "dayone" => array(
     "DEFAULT" => "storyone",
-    "storyone" => array(
-      "file" => "inc/day1.story.php"
-    )
+    "RELEASE" => "10/16/2013 3:50AM",
+    "storyone" => "inc/day1.story.php"
   ),
   "daytwo" => array(
     "DEFAULT" => "storytwo",
-    "storytwo" => array(
-      "file" => "inc/day2.story2.php"
-    )
+    "RELEASE" => "10/16/2013 3:30AM",
+    "storytwo" => "inc/day2.story2.php"
   )
 );
 
 $cur_part = "";
 $cur_story = "";
 
+// Set the part
 if(isset($_GET['part']) && !isset($stories[$_GET['part']]))
 {
   send_404();
@@ -32,6 +31,7 @@ else
   $cur_part = $_GET['part'];
 }
 
+// Set the story
 if(isset($_GET['story']) && !isset($stories[$cur_part][$_GET['story']]))
 {
   send_404();
@@ -45,6 +45,13 @@ else
   $cur_story = $_GET['story'];
 }
 
+// If the part isn't available, serve a 404
+if(time() < strtotime($stories[$cur_part]["RELEASE"]) && !DEBUG)
+{
+  send_404();
+}
+
+// Figure out the canonical URL for SEO
 $canonical_url = SITE_URL.$cur_part."/".$cur_story."/";
 if($cur_part == $stories["DEFAULT"] && $cur_story == $stories[$stories["DEFAULT"]]["DEFAULT"])
 {
@@ -54,6 +61,6 @@ if($cur_part == $stories["DEFAULT"] && $cur_story == $stories[$stories["DEFAULT"
 // Render the page
 include('inc/header.php'); 
 
-include($stories[$cur_part][$cur_story]["file"]);
+include($stories[$cur_part][$cur_story]);
 
 include('inc/footer.php'); ?>
