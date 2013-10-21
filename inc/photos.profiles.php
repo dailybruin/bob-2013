6548f14bd2811cf2c7aced058a60274bd60f02c1
Byron Lutz <byronlutz@gmail.com>
2013-10-21 02:19:33 -0700

    More photos in the gallery. This should go pretty quickly from here out."

--- inc/photos.profiles.php
+++ inc/photos.profiles.php
@@ -22,12 +22,25 @@
     </div>
     <div class="large-3 columns">
       <div id="quote1" class="quote">
-        <span class="lquote">&ldquo;</span>Nobody in our family knows that I am like this.<span class="rquote">&rdquo;</span>
-      </div>
-      <div id="quote2" class="quote">
-        <span class="lquote">&ldquo;</span>I fear to tell my sisters about my sexual orientation … Because if I tell them, they will tell my dad, and I will be evicted, so that’s the only fear I have.<span class="rquote">&rdquo;</span>
+        <span class="lquote">&ldquo;</span>When I was starting, it was difficult to understand myself. <span class="quote-continued">I was feeling as if maybe, am I mad, or maybe something is happening to me. Then as I was growing I just understand myself, that I am who I am … and I have to be who I am.<span class="rquote">&rdquo;</span></div>
       </div>
     </div>
   </div><!-- end div.row -->
+  
+  <div class="row photo-pablo.JPG">
+    <div class="large-12 columns">
+      <h3>Nelson &amp; Pablo</h3>
+    </div>
+    <div class="large-7 columns">
+      <div id="quote2" class="quote">
+        <span class="lquote">&ldquo;</span>We always keep our distance even for fear of being stigmatized. … I want to live in a society where everyone minds his own business.<span class="rquote">&rdquo;</span>
+        <span class="credit">— Pablo</span>
+      </div>
+    </div>
+    <div class="large-5 columns">
+      <img src="/img/photo/02.Nelson.Pablo.JPG">
+    </div>
+  </div><!-- end div.row -->
+  
 </article>
 <div class="clear"></div>
