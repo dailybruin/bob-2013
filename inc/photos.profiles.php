8a49065f2d0b9a5e9b7175feb073fb1bfd0f2125
Byron Lutz <byronlutz@gmail.com>
2013-10-21 07:39:57 -0700

    Added last photos to portraits page

--- inc/photos.profiles.php
+++ inc/photos.profiles.php
@@ -3,16 +3,7 @@
     <h2>Revealing Voices</h2>
     <div class="head-info"><span class="byline">By Blaine Ohigashi</span> • <span class="date">October 21, 2013</span></div>
   </div>
-  
-  <div class="subhead">
-    <hr />
-    <div class="row">
-      <div class="large-4 large-centered columns">
-        This is the first day of the trip. Put some information here that will lead people into reading the story.
-      </div>
-    </div>
-  </div>
-    
+      
   <div class="row photo-skinny">
     <div class="large-12 columns">
       <h3>Skinny</h3>
@@ -24,6 +15,36 @@
       <div id="quote1" class="quote">
         <span class="lquote">&ldquo;</span>When I was starting, it was difficult to understand myself. <span class="quote-continued">I was feeling as if maybe, am I mad, or maybe something is happening to me. Then as I was growing I just understand myself, that I am who I am … and I have to be who I am.<span class="rquote">&rdquo;</span></span>
         <span class="credit">— Skinny</span>
+      </div>
+    </div>
+  </div><!-- end div.row -->
+  
+  <div class="row photo-chipo">
+    <div class="large-12 columns">
+      <h3>Chipo</h3>
+    </div>
+    <div class="large-7 columns">
+      <div id="quote10" class="quote">
+        <span class="lquote">&ldquo;</span>You are a rejected person. … People will look at you as if you are evil.<span class="rquote">&rdquo;</span></span>
+        <span class="credit">— Chipo</span>
+      </div>
+    </div>
+    <div class="large-5 columns">
+      <img src="/img/photo/10.Chipo.jpg">
+    </div>
+  </div><!-- end div.row -->
+  
+  <div class="row photo-fortune">
+    <div class="large-12 columns">
+      <h3>Fortune</h3>
+    </div>
+    <div class="large-8 columns">
+      <img src="/img/photo/11.Fortune.jpg">
+    </div>
+    <div class="large-4 columns">
+      <div id="quote11" class="quote">
+        <span class="lquote">&ldquo;</span>I thought I was the only person like that in the whole world.<span class="rquote">&rdquo;</span></span>
+        <span class="credit">— Fortune</span>
       </div>
     </div>
   </div><!-- end div.row -->
