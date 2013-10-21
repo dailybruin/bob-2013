07505f58db9bcdafefc55105d0580ac039618f88
Byron Lutz <byronlutz@gmail.com>
2013-10-21 03:08:02 -0700

    Profiles photo gallery is done

--- inc/photos.profiles.php
+++ inc/photos.profiles.php
@@ -58,5 +58,90 @@
     </div>
   </div><!-- end div.row -->
   
+  <div class="row photo-madaliso">
+    <div class="large-12 columns">
+      <h3>Madaliso</h3>
+    </div>
+    <div class="large-12 columns">
+      <div id="quote4" class="quote">
+        <span class="lquote">&ldquo;</span>If they start talking shit, I just leave the place. I have nothing to say. … Maybe they say, ‘Look at this girl, as beautiful as she is, perfectly fair woman, what is it that you find in that?’ … they sometimes say we are satanic.’<span class="rquote">&rdquo;</span>
+        <span class="credit">— Madaliso</span>
+      </div>
+    </div>
+    <div class="large-12 columns">
+      <img src="/img/photo/04.Madaliso.JPG">
+    </div>
+  </div><!-- end div.row -->
+  
+  <div class="row photo-desire">
+    <div class="large-12 columns">
+      <h3>Desire</h3>
+    </div>
+    <div class="large-6 columns">
+      <div id="quote5" class="quote">
+        <span class="quote-continued">Standing outside his workplace in Mzuzu, a northern city in Malawi, Desire spoke in Chichewa about his job as a peer educator – he became friends with his first client at the market, where the man sold secondhand clothes. Desire found out he was gay after they became close friends and their trust increased.</span> He is a peer educator because, as a gay man, Desire thinks it is important to share messages of safety among the community.
+      </div>
+    </div>
+    <div class="large-6 columns">
+      <img src="/img/photo/05.Desire.JPG">
+    </div>
+  </div><!-- end div.row -->
+  
+  <div class="row photo-nelson">
+    <div class="large-12 columns">
+      <h3>Nelson</h3>
+    </div>
+    <div class="large-7 columns">
+      <img src="/img/photo/06.Nelson.JPG">
+    </div>
+    <div class="large-5 columns">
+      <div id="quote6" class="quote">
+        <span class="lquote">&ldquo;</span>I also wish if I had the opportunity to do that in public just like everyone else … I wish we could move to some place.<span class="rquote">&rdquo;</span> <span class="quote-continued">Nelson wishes he and his boyfriend could hold hands in public and be intimate with each other.</span></div>
+    </div>
+  </div><!-- end div.row -->
+  
+  <div class="row photo-mwayi">
+    <div class="large-12 columns">
+      <h3>Mwayi</h3>
+    </div>
+    <div class="large-3 columns">
+      <div id="quote7" class="quote">
+        <span class="lquote">&ldquo;</span>If I go for prayers, maybe they might really, really pray for me, and I will lose my girlfriend. … I love my girlfriend very much. I don’t want to lose her.<span class="rquote">&rdquo;</span> <span class="quote-continued">Mwayi and her girlfriend have been dating for six years. She stopped praying for her attraction to women to go away after they started dating.</span></div>
+    </div>
+    <div class="large-9 columns">
+      <img src="/img/photo/07.Mwayi.JPG">
+    </div>
+  </div><!-- end div.row -->
+
+  <div class="row photo-lucky">
+    <div class="large-12 columns">
+      <h3>Lucky</h3>
+    </div>
+    <div class="large-9 columns">
+      <img src="/img/photo/08.Lucky.JPG">
+    </div>
+    <div class="large-3 columns">
+      <div id="quote8" class="quote">
+        Lucky lives in the northern region of Malawi, where he is a peer educator. He has many gay friends and says it’s easy to spot another gay man. Once he figures out that someone is a man who has sex with men, he will approach him, maybe in a bar, become his friend and then bring up his role and confirm the man’s orientation. That’s how he builds his network and spreads HIV-related messages.
+      </div>
+    </div>
+  </div><!-- end div.row -->
+
+  <div class="row photo-khumbo">
+    <div class="large-12 columns">
+      <h3>Khumbo</h3>
+    </div>
+    <div class="large-4 columns">
+      <div id="quote9" class="quote">
+        <span class="lquote">&ldquo;</span>It’s very hard to come open, to tell your family, <span class="quote-continued">because you never know what their reaction will be, because it’s illegal here in Malawi. So that’s why I’m afraid to tell them. But if the time comes, I hope that I will tell them. … I’m just waiting for them to hear from people talking, talking, talking about the LGBT. … So it will not be a new thing to them.<span class="rquote">&rdquo;</span></span> 
+        <span class="credit">— Khumbo</span>       
+      </div>
+    </div>
+    <div class="large-8 columns">
+      <img src="/img/photo/09.Khumbo.JPG">
+    </div>
+  </div><!-- end div.row -->
+
+  
 </article>
 <div class="clear"></div>
