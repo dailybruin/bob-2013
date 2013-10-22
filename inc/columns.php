9ece86d2408c1c34289ff4aa7d537eb9b2f36abe
Byron Lutz <byronlutz@gmail.com>
2013-10-22 04:19:10 -0700

    Column text added

--- inc/columns.php
+++ inc/columns.php
@@ -4,41 +4,38 @@
       <h2 class="column-head">This is the headline for Sonali&rsquo;s column</h2>
       <div class="column-info"><span class="byline">By Sonali Kohli</span> • October 23, 2013</div>
       <img src="<?php echo SITE_URL; ?>img/day3/sonali.jpg" class="mugshot" />
-      <p class="dropcap">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac lacus vitae velit pharetra rhoncus. In non lectus vel quam egestas consequat. Vivamus hendrerit quam sit amet lobortis tincidunt. Nam congue, mi sit amet pretium convallis, elit felis ultricies justo, vitae accumsan velit tortor vel turpis. Proin ultricies vehicula dolor, ac volutpat urna pellentesque laoreet. Donec eu cursus ipsum, a tincidunt arcu. Aliquam sed porta tellus. Nulla facilisi. Nullam non bibendum dui, vitae laoreet diam. Sed enim erat, porttitor sit amet ante id, euismod cursus nunc.
-      </p>
-      <p>
-      Fusce sed faucibus turpis, at malesuada dolor. Nam suscipit pretium orci. Morbi vitae libero pulvinar, dapibus nisl nec, adipiscing arcu. Morbi mollis ante id erat blandit molestie. Etiam dignissim ultrices leo, eu vulputate lorem consequat et. Cras quis magna ac tellus faucibus placerat in id sapien. Vivamus orci sem, lobortis sed laoreet ut, suscipit id nulla. In rhoncus lacus ac malesuada varius. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-      </p>
-      <p>
-      Praesent hendrerit id lorem sit amet porttitor. Morbi a ipsum scelerisque, ultrices elit et, porta sapien. Aenean purus purus, adipiscing at diam sit amet, scelerisque consequat turpis. Donec ornare erat felis, eget tincidunt mi facilisis lacinia. Etiam eu sodales leo, porttitor vestibulum orci. Duis fermentum tempus ante et congue. Donec nec accumsan quam, non egestas ante. Integer pellentesque eros et placerat suscipit. Fusce eleifend interdum ultrices. Morbi viverra condimentum odio, nec consectetur nunc gravida a. Integer iaculis neque ligula, ut molestie nunc vehicula et. Sed sed odio viverra, blandit neque sit amet, fringilla diam. Donec faucibus vulputate luctus.
-      </p>
-      <p>
-      Aliquam porta orci risus, a aliquet diam pulvinar et. Proin nec mattis mi. Nulla vulputate nisl nec leo mollis ultrices quis quis ipsum. Duis rutrum a risus sit amet dapibus. Mauris nec venenatis metus. Duis vehicula ipsum eu scelerisque interdum. Sed ultrices tempor magna a vehicula. Nulla facilisi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc hendrerit lectus vitae semper tincidunt. Vivamus augue erat, suscipit sed dapibus a, consequat sit amet nisl.
-      </p>
-      <p>
-      Etiam id metus at nunc vestibulum iaculis. Nam eget pulvinar lectus. Aenean lacus lorem, volutpat quis luctus quis, venenatis eget risus. Pellentesque dignissim enim tortor, at dictum sapien mollis ac. Praesent auctor mauris rutrum metus bibendum porttitor. Proin a lacinia lacus. Maecenas vitae velit a tortor dignissim posuere vitae eget arcu. Ut rhoncus felis lacinia urna interdum, congue sagittis sapien sagittis. Duis tincidunt adipiscing ultrices. Curabitur id lorem vel ipsum porttitor suscipit non id tortor. Duis magna tortor, fermentum nec imperdiet id, ornare nec libero. Pellentesque metus libero, consequat sit amet consequat at, eleifend vel lorem.
-      </p>
+      <p class="dropcap">I asked him if there was anything else he thought I should know about his story, or any identifying details to leave out.</p>
+      <p>He said in Chichewa that he was afraid he would be arrested again, or that he and his boyfriend would be discovered.</p>
+      <p>He said he was trusting me to keep his story safe, and he hoped I wouldn’t put him in danger.</p>
+      <p>Usually, my job as a reporter is to convince a person to let me publish as many details of their story as possible, because I can weave those details into a narrative that will help a reader connect to and better understand the issue.</p>
+      <p>But while reporting on the lesbian, gay, bisexual and transgender community in Malawi, I often found myself doing the opposite. At the end of each interview, I combed through my notes and pointed out details that I thought might be too revealing, and explained again that although these stories were publishing in my university’s newspaper, they would also be online, and anyone in the world could access them.</p>
+      <p>And in parsing through the details I could use for the stories I had to make judgments about what it means to keep someone’s identity a secret. Is it as simple as withholding their full name? Their first name? The town they live in? A description of their clothes?</p>
+      <p>I promised the source that I wouldn’t reveal his name, or anything else he thought would make people recognize him.</p>
+      <p>And then we drove away.</p>
+      <p>Over 24 days in Malawi, we heard the stories of people whose families found out about their orientation – some were arrested, some were beaten, some were accepted, at least in part.</p>
+      <p>In Malawi, my word was the only assurance I could give people that I wouldn’t reveal their identities. </p>
+      <p>Malawi is not the most dangerous place in the world in terms of gay rights. People were comfortable enough that they would meet us and talk to us, and we could write these stories.</p>
+      <p>But what about environments that are so harsh that the risks associated with talking to a reporter far outweigh the intangible benefits of a news article? As journalists, how do we report those stories?</p>
+      <p>I don’t have a solution to those questions, and I don’t know if I will any time soon. But I know I want to try, because those are the most important stories to tell. ■</p>
     </div>
     <div class="large-7 columns story">
       <h2 class="column-head">This is the headline for Blaine's column</h2>
       <div class="column-info"><span class="byline">By Blaine Ohigashi</span> • October 23, 2013</div>
       <img src="<?php echo SITE_URL; ?>img/day3/blaine.jpg" class="mugshot mugshot-small" />
-      <p class="dropcap">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac lacus vitae velit pharetra rhoncus. In non lectus vel quam egestas consequat. Vivamus hendrerit quam sit amet lobortis tincidunt. Nam congue, mi sit amet pretium convallis, elit felis ultricies justo, vitae accumsan velit tortor vel turpis. Proin ultricies vehicula dolor, ac volutpat urna pellentesque laoreet. Donec eu cursus ipsum, a tincidunt arcu. Aliquam sed porta tellus. Nulla facilisi. Nullam non bibendum dui, vitae laoreet diam. Sed enim erat, porttitor sit amet ante id, euismod cursus nunc.
-      </p>
-      <p>
-      Fusce sed faucibus turpis, at malesuada dolor. Nam suscipit pretium orci. Morbi vitae libero pulvinar, dapibus nisl nec, adipiscing arcu. Morbi mollis ante id erat blandit molestie. Etiam dignissim ultrices leo, eu vulputate lorem consequat et. Cras quis magna ac tellus faucibus placerat in id sapien. Vivamus orci sem, lobortis sed laoreet ut, suscipit id nulla. In rhoncus lacus ac malesuada varius. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-      </p>
-      <p>
-      Praesent hendrerit id lorem sit amet porttitor. Morbi a ipsum scelerisque, ultrices elit et, porta sapien. Aenean purus purus, adipiscing at diam sit amet, scelerisque consequat turpis. Donec ornare erat felis, eget tincidunt mi facilisis lacinia. Etiam eu sodales leo, porttitor vestibulum orci. Duis fermentum tempus ante et congue. Donec nec accumsan quam, non egestas ante. Integer pellentesque eros et placerat suscipit. Fusce eleifend interdum ultrices. Morbi viverra condimentum odio, nec consectetur nunc gravida a. Integer iaculis neque ligula, ut molestie nunc vehicula et. Sed sed odio viverra, blandit neque sit amet, fringilla diam. Donec faucibus vulputate luctus.
-      </p>
-      <p>
-      Aliquam porta orci risus, a aliquet diam pulvinar et. Proin nec mattis mi. Nulla vulputate nisl nec leo mollis ultrices quis quis ipsum. Duis rutrum a risus sit amet dapibus. Mauris nec venenatis metus. Duis vehicula ipsum eu scelerisque interdum. Sed ultrices tempor magna a vehicula. Nulla facilisi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc hendrerit lectus vitae semper tincidunt. Vivamus augue erat, suscipit sed dapibus a, consequat sit amet nisl.
-      </p>
-      <p>
-      Etiam id metus at nunc vestibulum iaculis. Nam eget pulvinar lectus. Aenean lacus lorem, volutpat quis luctus quis, venenatis eget risus. Pellentesque dignissim enim tortor, at dictum sapien mollis ac. Praesent auctor mauris rutrum metus bibendum porttitor. Proin a lacinia lacus. Maecenas vitae velit a tortor dignissim posuere vitae eget arcu. Ut rhoncus felis lacinia urna interdum, congue sagittis sapien sagittis. Duis tincidunt adipiscing ultrices. Curabitur id lorem vel ipsum porttitor suscipit non id tortor. Duis magna tortor, fermentum nec imperdiet id, ornare nec libero. Pellentesque metus libero, consequat sit amet consequat at, eleifend vel lorem.
-      </p>
+      <p class="dropcap">I was trying to put my anonymous subject at ease, assuring her that the portrait I took of her would not reveal her identity. </p>
+      <p>I saw the photo merely as a tight, over-the-shoulder shot of her earring. But to her it was more. She was unable to put into words exactly why she thought she could be identified from the photo and all she could say was, “That’s me. Yes, that is me.”</p>
+      <p>I was taken aback by this statement, and began to rethink all the portraits I had shot in the country until that point.</p>
+      <p>I first saw this project as an opportunity to make images that could tell a story reaching beyond the bounds of the UCLA community. But after arriving in the country and embarking upon this photographic journey, I sometimes felt overwhelmed trying to juggle my subjects’ safety and the quality of storytelling in each photo.</p>
+      <p>As a photojournalist, I strive to tell stories through meaningful images that resonate with viewers. Unlike my past work, however, for this series, the safety of my subjects was paramount, and I needed to protect their identities – not reveal them.</p>
+      <p>The members of the LGBT community I was photographing were hiding in plain sight, and I was undertaking the seemingly paradoxical task of making photos that encapsulated who these people were without actually showing any physical features that could expose their identities. Doing that in an elegant way was a challenge.</p>
+      <p>How could I be sure that I was successfully hiding their identities?</p>
+      <p>We let sources decide where and when we met to help maintain their anonymity and safety. I worked with the available lighting and locations in order to compose anonymous portraits – that  entailed everything from shooting through the slats of a wooden fence, to taking advantage of the shadows cast by a city streetlight at night.</p>
+      <p>I made it work, and all of my subjects were okay with their photos. But now, in one of our last interviews of the trip, someone challenged me.</p>
+      <p>Photojournalism allows me to contribute positive change to the world by bringing informative images of stories, both big and small, to the masses. But in Malawi I was forced to think about who was best served by this contribution.  </p>
+      <p>Was I telling these stories to impact the way a country’s population regards the LGBT community? Was I telling these stories to help individuals who might not have the means to tell their own stories? Or was I telling these stories for myself? This was the first time I had thought about photojournalism so deeply, and I determined that I was doing a little bit of each. And it is for these reasons that I’ve allowed photojournalism to consume my life for the past five years. </p>
+      <p>I do think photojournalism has the power to captivate a generation, but that’s a rare occurrence. I’m not naïve enough to think I achieved this in Malawi. </p>
+      <p>But I did connect with people on a personal and emotional level. The individuals we talked to have hidden their sexuality from their friends and families for years, but they made the decision to share their stories with us, complete strangers. </p>
+      <p>Words on a page aren’t enough to do their stories justice. I can’t share the full identities of these Malawian men and women, but I hope my photos helped bring their stories to light.” ■</p>
     </div>
   </div>
 </article>
