3dd3ea13b2779e12bb5ab38c1266dbf714ffec68
Byron Lutz <byronlutz@gmail.com>
2013-10-22 03:31:08 -0700

    Finished timeline

--- inc/day3.story2.php
+++ inc/day3.story2.php
@@ -13,94 +13,32 @@
   </div>
       
   <div class="row">
-    <div class="large-3 columns">
-      <div id="about-bridget">
-        <p><strong>Editor's note</strong></p>
-        <p>All of the LGBT men and women in these stories were kept anonymous in this series because of the danger that the laws and heavy social stigma pose in Malawi. The journalists asked each anonymous person to choose the name that appears in the stories.</p>
-      </div><!-- end div#about-bridget -->
-    </div><!-- end div.large-3 -->
-    <div class="large-7 columns story story-drop">
-      <p>The couple sat side by side in a Malawian coffee shop, both sets of arms crossed on top of the table, no parts of them touching. </p>
-      <p>One of the men, who asked to be called Pablo for this story, has been clean for a year – he credits that to his boyfriend of six years, who asked him to stop using drugs and drinking for the sake of their relationship. </p>
-      <figure>
-        <img src="<?php echo SITE_URL ?>img/photo/02.Nelson.Pablo.JPG" />
-        <figcaption>
-          <span class="credit">Blaine Ohigashi / Daily Bruin Senior Staff</span>
-          &ldquo;We always keep our distance even for fear of being stigmatized. &hellip; I want to live in a society where everyone minds his own business.&rdquo; &mdash;&nbsp;Pablo
-        </figcaption>
-      </figure>
-      <p>Malawi is a small country tucked into southeastern Africa, between Tanzania, Mozambique and Zambia. Homosexuality is both illegal and heavily stigmatized, so Pablo and his boyfriend have to be cautious about the way they act in public. Most people assume they’re friends or cousins.</p>
-      <p>In Malawi, few resources exist to combat the underlying mental health problems that can lead to addiction for the lesbian, gay, bisexual and transgender community.</p>
-      <p>Members of the LGBT community face higher rates of substance abuse than the general population worldwide because they tend to experience higher levels of stress, said Ilan Meyer, a UCLA law professor who has researched mental health within the LGBT community for the Williams Institute at UCLA, which conducts law and public policy research on the LGBT community.</p>
-      <p>The United States Agency for International Development committed $96.8 million in aid to Malawi during the 2013 fiscal year. The biggest chunk of that – $26.9 million – went to combatting HIV/AIDS. None of it went to what is classified as “democracy, human rights axnd governance.” </p>
-      <p>Most of the funding to decrease addiction in the LGBT community is dedicated to preventing HIV/AIDS by stopping the drug and alcohol abuse that leads to risky sexual behaviors – not by addressing the psychological sources of that addiction.</p>
-      <div class="flex-video">
-        <iframe width="420" height="315" src="//www.youtube.com/embed/URKGIa0b_jI" frameborder="0" allowfullscreen></iframe> 
-      </div>
-      <p>As of July, there were only two licensed clinical psychologists in all of Malawi, said Chiwoza Bandawe, one of those psychologists and the dean of students at the University of Malawi College of Medicine. </p>
-      <p>Add to that limited funding, and the LGBT community is left with limited access to adequate mental health and counseling services, Bandawe said.</p>
-      <p></p>
-      <p><strong>Looking for a safe place</strong></p>
-      <figure>
-        <img src="<?php echo SITE_URL ?>img/day3/web.bob.10.23.mentalhealth.picA.jpg" />
-        <figcaption>
-          <span class="credit">Blaine Ohigashi / Daily Bruin Senior Staff</span>
-          ??????
-        </figcaption>
-      </figure>
-      <p>Mental health resources for LGBT communities are greater in countries with more gay rights, such as the United States, Meyer said.</p>
-      <p>UCLA, for example, has an LGBT resource center centrally located on campus, near Bruin Plaza. It has an in-house counselor and a library.   The university has had some form of an LGBT student center since 1995.</p>
-      <p>In Malawi, however, the LGBT community lacks a place where they can ask questions, find educational resources and seek counseling and legal aid, according to a needs assessment study by Bandawe and the Centre for the Development of People, the country’s primary LGBT rights organization, commonly referred to as CEDEP.</p>
-      <p>Without counseling, gay men and women are often alone in facing self-hatred and the threat of losing their jobs and families if found out. Some choose to try conversion attempts or exorcisms.</p>
-      <p>Constantly hiding their identity from families, friends and employers can take a mental toll on gay men and lesbian women and lead to a dependence on alcohol or drugs, Bandawe said.</p>
-      <p>Problems with addiction are only exacerbated by Malawi’s high unemployment rate and a drinking culture that starts in the morning and provides easy access to small plastic bags of 40 percent alcohol for the equivalent of about 15 U.S. cents. </p>
-      <p>“I would actually spend the whole night in the club, and I would drink too much whiskey,” Pablo said. </p>
-      <p>Pablo drank and went out because that’s how most of the men he surrounded himself with spent their time. </p>
-      <p>He was addicted.</p>
-      <p>He was able to stop because he had encouragement from his boyfriend and a supportive family in Zimbabwe, where the culture is more open toward homosexuality. Meanwhile, full-time employment at CEDEP keeps him busy so that he doesn’t spend his time drinking.</p>
-      <figure>
-        <img src="<?php echo SITE_URL ?>img/day3/10.22.copyeditedBOB.PEPFAR.jpg" />
-      </figure>
-      <p>But after Pablo became sober, he noticed that some of his friends were still abusing.</p>
-      <p>One program Pablo is involved in with CEDEP includes a network of peer educators who share messages about HIV/AIDS prevention with the community, which mostly consists of men who have sex with men.  Earlier this year, they taught that drug and alcohol abuse in an already high-risk community can make an easy transmission even easier because men who are consistently high or drunk might share needles or be more likely to have unprotected sex.</p>
-      <p>Pablo had pushed for this message.</p>
-      <p>“I’ve been clean since last year August. So I used an example of myself,” Pablo said. “If it’s working out for me, I thought it wise to spread it out to my friends.”</p>
-      <p>A friend of Pablo’s, who asked to be called, Skinny has an alcohol problem. Skinny in one of the peer educators responsible for spreading CEDEP’s quarterly messages. A one-on-one talk in Pablo’s office earlier this year helped him reduce his drinking, he said.</p>
-      <p>Skinny is a soft-spoken man in his early 20s, who laughs through any part of a conversation that makes him uncomfortable. As of July, he had a boyfriend for two years. He used to take a variety of drugs and drink 10 to 15 bottles of beer a night. Falling asleep at the bar was common, he said.</p>
-      <p>Skinny reduced his drinking habits before spreading the message, he said in July. Now he drinks four to five bottles of beer, but only on Saturdays.</p>
-      <p>He has also encouraged some of his clients to get clean. In July, he said some of them had started drinking less, but others had kept up their habits after talking to him.</p>
-      <p>“But as time goes maybe they’ll change, because we don’t just send the message once,” Skinny said. </p>
-      <p>So he’ll continue to bring it up in conversation  – in the bars where he meets friends or when they come to him seeking the condoms and lubrication that CEDEP provides.</p>
-      <p></p>
-      <p><strong>A deeper problem</strong></p>
-      <p>Merely changing behavior, however, does not necessarily resolve the mental health issues that often lead to addiction. Members of Malawi’s LGBT community who have reduced their drug and alcohol usage still have to cope with the threat of arrest, the fear of being outed, and the stress of constant hiding. </p>
-      <p>During a July focus group about drug and alcohol abuse at CEDEP’s northern office, men talked about the reasons behind substance abuse in the LGBT community.</p>
-      <p>It’s easier to approach a man romantically when you’re drunk – the alcohol takes away the fear of rejection, the fear of arrest, the fear of fighting with someone.</p>
-      <p>Many people are unemployed, alcohol is cheap, and there’s nothing else to do.</p>
-      <p>Walking down the street is stressful when you know everyone is whispering about you, saying you’re gay, judging you.</p>
-      <p>But CEDEP does not currently have the funding to substantially expand mental health aid, CEDEP director Gift Trapence said in July.</p>
-      <p>“There is that gap that we’ve realized that we need to work on,” he said. “But of course now comes the issue of resources.” </p>
-      <p>Ideally, Trapence would like to be able to provide shelter to men and women who have been outed and kicked out of their homes, as well as counseling resources at the offices.</p>
-      <figure>
-        <img src="<?php echo SITE_URL ?>img/photo/03.Lane.JPG" />
-        <figcaption>
-          <span class="credit">Blaine Ohigashi / Daily Bruin Senior Staff</span>
-          This man, who asked to be called Lane for this story, is a Malawian who was kicked out of his house, and later beaten, by his family when they found out he was gay. He takes drugs to deail with the memory.
-        </figcaption>
-      </figure>
-      <p>Lane, a gay Malawian man who asked to remain anonymous, didn’t have a professional to turn to this summer when his family found out he was gay.</p>
-      <p>In June, his upper middle-class family kicked him out of the house and fired him from the family business after they saw his name on a Facebook page outing him and other Malawians who are gay and lesbian.</p>
-      <p>His family found out where he was staying later that month and came to the house at night.</p>
-      <p>They drove him to his uncle’s house, pinned him to the ground and gagged him with a cloth. Then, his mother, sister and brothers beat him for four hours, he said.</p>
-      <p>“It was like a nightmare that I was trying to wake up from, but I realized it was a reality,” he said.</p>
-      <p>The only thing that makes remembering that night easier, Lane said, is to get high. </p>
-      <p>He uses pot two or three times a week, plus cocaine once every one or two weeks, when he can get his hands on it.</p>
-      <p>He’s hoping to get asylum in another country, so he can pursue his flight attendant career.</p>
-      <p>But he needs money to pay for that school. As of September, he was still $1,000 short.</p>
-      <p>“All I need is my life back,” Lane said. “I want to achieve what I’ve been looking forward to.”  </p>
-      <p>Meanwhile, he said he would like access to a psychologist who can help him cope, to play the role that drugs do now.</p>
-      <p>Pablo, who works in the northern region of Malawi, would like to see the main room in the CEDEP northern office transformed into a library, a safe space where people can come to educate themselves on their rights and resources.</p>
-      <p>Right now, he said, he can only rely on his own experience to provide that help. And it’s not enough. ■</p>
+    <div class="large-7 large-offset-3 columns story story-drop">
+      <p>She didn’t try to stop the beating.</p>
+      <p>She cried through it, and it hurt. But she understood why it was happening.</p>
+      <p>Her father was angry that she was lesbian, he was angry that people knew, he was angry that he had heard through other people.</p>
+      <p>“I just let him,” said the Malawian woman in her 20s, who asked to be called Mwayi for this story.</p>
+      <p>Activists helped Mwayi find a home after her family disowned her in February. In July, she had been living in her current house for two months. </p>
+      <p>In the struggle to gain rights for lesbian, gay, bisexual and transgender Malawians, some members of that community seem to be left out.</p>
+      <p>The reason that lesbian women in Malawi have been sidelined for so long is largely due to funding. Activists go after international HIV/AIDS funding to help the community, but that aid is mostly targeted toward men who have sex with men because they have a high risk of contraction.  </p>
+      <p>Anal sex is more likely to lead to sexually transmitted infections than vaginal sex because the lining of the anus is much thinner. Men who have sex with men are more likely than lesbian women or heterosexual couples to engage in anal sex, so they are at higher risk of contracting HIV.</p>
+      <p>“(Activists) take the pulse of the population and lawmakers and religious groups to understand what’s appropriate at a particular time,” said Ashley Currier, an assistant professor of women’s, gender and sexuality studies at the University of Cincinnati who researches LGBT organizing and homophobia around the world. </p>
+      <p>Activists in some countries, such as South Africa, have managed to tailor their public health approach partially to women. They use HIV/AIDS funding to target sexual violence against women, saying that attacks like what activists call “corrective rape,” the practice of raping lesbian women to make them straight, contribute to the spread of HIV. LGBT populations have constitutional protections in South Africa, in contrast to Malawi’s restrictive laws.</p>
+      <p>UCLA has worked with the MAC AIDS Fund in South Africa to fund projects that help women who have been through these attacks. </p>
+      <p>In Malawi, the resources for lesbians are scarce.</p>
+      <p>Fewer Malawian women are educated than men, so they have fewer employment opportunities. 2010 literacy rates were estimated at 68 percent for women older than 15, in contrast to 81 percent for men older than 15, according to the World Bank.  </p>
+      <p>This means women are more dependent on their families and more likely than men to live at home until marriage, which makes it harder for some lesbians to come out.</p>
+      <p>The original law banning homosexual acts applied only to men. In 2011, the Malawian Parliament extended it to women as well, though women face up to five years in prison and men face up to 14 years with hard labor. </p>
+      <p>Activists make the case that if it is illegal for men to have sex with men, it is difficult to get them the medical services they need.</p>
+      <p>But the same argument can’t be made as strongly for women who have sex with women, so there are fewer opportunities to provide aid to women.</p>
+      <p>At times, activists provide individual aid if a woman needs it, like in Mwayi’s case, but women’s rights organizations do not tailor their goals toward lesbians.</p>
+      <p>Instead, those groups are largely focused on overall violence toward women, or toward empowering straight women to demand condom use and consistent HIV/AIDS testing from their male partners.</p>
+      <p>Oberlin College anthropology professor Crystal Biruk is writing a book about lesbians in Malawi, with Gift Trapence, the director of Malawi's primary gay rights organization, the Centre for the Development of People, also known as CEDEP.  This would be one of the first pieces of comprehensive research regarding lesbians in Malawi.</p>
+      <p>But Mwayi wants more, even though she doesn’t know how realistic her hopes are.</p>
+      <p>She said lesbian women in the country should have access to scholarships for college tuition, as well as help getting jobs, so they have the independence to come out and live as lesbian women.</p>
+      <p>CEDEP is also trying to create programs for lesbian women and prove that they exist in Malawi. The organization was founded in 2005 by a group of men who originally focused on gaps in HIV/AIDS treatment, so women were left out of the original strategic plan, said CEDEP administrator Dunker Kamba.</p>
+      <p>Earlier this year, about 40 Malawian lesbians met with CEDEP to talk about their needs and rights.</p>
+      <p>Mwayi was there, advocating for herself.</p>
       </p>
     </div>
     <div class="large-2 columns"> </div>
