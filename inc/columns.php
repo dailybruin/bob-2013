7ffc72681d2401b30e859db819ffa2208207bff4
Byron Lutz <byronlutz@gmail.com>
2013-10-22 04:12:03 -0700

    Columns are now styled properly

---
+++ inc/columns.php
@@ -0,0 +1,46 @@
+<article id="trip-columns">
+  <div class="row">
+    <div class="large-5 columns story">
+      <h2 class="column-head">This is the headline for Sonali&rsquo;s column</h2>
+      <div class="column-info"><span class="byline">By Sonali Kohli</span> • October 23, 2013</div>
+      <img src="<?php echo SITE_URL; ?>img/day3/sonali.jpg" class="mugshot" />
+      <p class="dropcap">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac lacus vitae velit pharetra rhoncus. In non lectus vel quam egestas consequat. Vivamus hendrerit quam sit amet lobortis tincidunt. Nam congue, mi sit amet pretium convallis, elit felis ultricies justo, vitae accumsan velit tortor vel turpis. Proin ultricies vehicula dolor, ac volutpat urna pellentesque laoreet. Donec eu cursus ipsum, a tincidunt arcu. Aliquam sed porta tellus. Nulla facilisi. Nullam non bibendum dui, vitae laoreet diam. Sed enim erat, porttitor sit amet ante id, euismod cursus nunc.
+      </p>
+      <p>
+      Fusce sed faucibus turpis, at malesuada dolor. Nam suscipit pretium orci. Morbi vitae libero pulvinar, dapibus nisl nec, adipiscing arcu. Morbi mollis ante id erat blandit molestie. Etiam dignissim ultrices leo, eu vulputate lorem consequat et. Cras quis magna ac tellus faucibus placerat in id sapien. Vivamus orci sem, lobortis sed laoreet ut, suscipit id nulla. In rhoncus lacus ac malesuada varius. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      </p>
+      <p>
+      Praesent hendrerit id lorem sit amet porttitor. Morbi a ipsum scelerisque, ultrices elit et, porta sapien. Aenean purus purus, adipiscing at diam sit amet, scelerisque consequat turpis. Donec ornare erat felis, eget tincidunt mi facilisis lacinia. Etiam eu sodales leo, porttitor vestibulum orci. Duis fermentum tempus ante et congue. Donec nec accumsan quam, non egestas ante. Integer pellentesque eros et placerat suscipit. Fusce eleifend interdum ultrices. Morbi viverra condimentum odio, nec consectetur nunc gravida a. Integer iaculis neque ligula, ut molestie nunc vehicula et. Sed sed odio viverra, blandit neque sit amet, fringilla diam. Donec faucibus vulputate luctus.
+      </p>
+      <p>
+      Aliquam porta orci risus, a aliquet diam pulvinar et. Proin nec mattis mi. Nulla vulputate nisl nec leo mollis ultrices quis quis ipsum. Duis rutrum a risus sit amet dapibus. Mauris nec venenatis metus. Duis vehicula ipsum eu scelerisque interdum. Sed ultrices tempor magna a vehicula. Nulla facilisi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc hendrerit lectus vitae semper tincidunt. Vivamus augue erat, suscipit sed dapibus a, consequat sit amet nisl.
+      </p>
+      <p>
+      Etiam id metus at nunc vestibulum iaculis. Nam eget pulvinar lectus. Aenean lacus lorem, volutpat quis luctus quis, venenatis eget risus. Pellentesque dignissim enim tortor, at dictum sapien mollis ac. Praesent auctor mauris rutrum metus bibendum porttitor. Proin a lacinia lacus. Maecenas vitae velit a tortor dignissim posuere vitae eget arcu. Ut rhoncus felis lacinia urna interdum, congue sagittis sapien sagittis. Duis tincidunt adipiscing ultrices. Curabitur id lorem vel ipsum porttitor suscipit non id tortor. Duis magna tortor, fermentum nec imperdiet id, ornare nec libero. Pellentesque metus libero, consequat sit amet consequat at, eleifend vel lorem.
+      </p>
+    </div>
+    <div class="large-7 columns story">
+      <h2 class="column-head">This is the headline for Blaine's column</h2>
+      <div class="column-info"><span class="byline">By Blaine Ohigashi</span> • October 23, 2013</div>
+      <img src="<?php echo SITE_URL; ?>img/day3/blaine.jpg" class="mugshot mugshot-small" />
+      <p class="dropcap">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac lacus vitae velit pharetra rhoncus. In non lectus vel quam egestas consequat. Vivamus hendrerit quam sit amet lobortis tincidunt. Nam congue, mi sit amet pretium convallis, elit felis ultricies justo, vitae accumsan velit tortor vel turpis. Proin ultricies vehicula dolor, ac volutpat urna pellentesque laoreet. Donec eu cursus ipsum, a tincidunt arcu. Aliquam sed porta tellus. Nulla facilisi. Nullam non bibendum dui, vitae laoreet diam. Sed enim erat, porttitor sit amet ante id, euismod cursus nunc.
+      </p>
+      <p>
+      Fusce sed faucibus turpis, at malesuada dolor. Nam suscipit pretium orci. Morbi vitae libero pulvinar, dapibus nisl nec, adipiscing arcu. Morbi mollis ante id erat blandit molestie. Etiam dignissim ultrices leo, eu vulputate lorem consequat et. Cras quis magna ac tellus faucibus placerat in id sapien. Vivamus orci sem, lobortis sed laoreet ut, suscipit id nulla. In rhoncus lacus ac malesuada varius. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      </p>
+      <p>
+      Praesent hendrerit id lorem sit amet porttitor. Morbi a ipsum scelerisque, ultrices elit et, porta sapien. Aenean purus purus, adipiscing at diam sit amet, scelerisque consequat turpis. Donec ornare erat felis, eget tincidunt mi facilisis lacinia. Etiam eu sodales leo, porttitor vestibulum orci. Duis fermentum tempus ante et congue. Donec nec accumsan quam, non egestas ante. Integer pellentesque eros et placerat suscipit. Fusce eleifend interdum ultrices. Morbi viverra condimentum odio, nec consectetur nunc gravida a. Integer iaculis neque ligula, ut molestie nunc vehicula et. Sed sed odio viverra, blandit neque sit amet, fringilla diam. Donec faucibus vulputate luctus.
+      </p>
+      <p>
+      Aliquam porta orci risus, a aliquet diam pulvinar et. Proin nec mattis mi. Nulla vulputate nisl nec leo mollis ultrices quis quis ipsum. Duis rutrum a risus sit amet dapibus. Mauris nec venenatis metus. Duis vehicula ipsum eu scelerisque interdum. Sed ultrices tempor magna a vehicula. Nulla facilisi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc hendrerit lectus vitae semper tincidunt. Vivamus augue erat, suscipit sed dapibus a, consequat sit amet nisl.
+      </p>
+      <p>
+      Etiam id metus at nunc vestibulum iaculis. Nam eget pulvinar lectus. Aenean lacus lorem, volutpat quis luctus quis, venenatis eget risus. Pellentesque dignissim enim tortor, at dictum sapien mollis ac. Praesent auctor mauris rutrum metus bibendum porttitor. Proin a lacinia lacus. Maecenas vitae velit a tortor dignissim posuere vitae eget arcu. Ut rhoncus felis lacinia urna interdum, congue sagittis sapien sagittis. Duis tincidunt adipiscing ultrices. Curabitur id lorem vel ipsum porttitor suscipit non id tortor. Duis magna tortor, fermentum nec imperdiet id, ornare nec libero. Pellentesque metus libero, consequat sit amet consequat at, eleifend vel lorem.
+      </p>
+    </div>
+  </div>
+</article>
+
+<div class="clear"></div>
