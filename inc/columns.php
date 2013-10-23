68bde51af5c044e4a518507bf8a9b3229c3a2c12
Byron Lutz <byronlutz@gmail.com>
2013-10-23 03:27:32 -0700

    Replaced column text and added headlines

--- inc/columns.php
+++ inc/columns.php
@@ -1,7 +1,7 @@
 <article id="trip-columns">
   <div class="row">
     <div class="large-5 columns story">
-      <h2 class="column-head">This is the headline for Sonali&rsquo;s column</h2>
+      <h2 class="column-head">Balancing anonymity, reporting</h2>
       <div class="column-info"><span class="byline">By Sonali Kohli</span> • October 23, 2013</div>
       <img src="<?php echo SITE_URL; ?>img/day3/sonali.jpg" class="mugshot" />
       <p class="dropcap">I asked him if there was anything else he thought I should know about his story, or any identifying details to leave out.</p>
@@ -13,29 +13,29 @@
       <p>I promised the source that I wouldn’t reveal his name, or anything else he thought would make people recognize him.</p>
       <p>And then we drove away.</p>
       <p>Over 24 days in Malawi, we heard the stories of people whose families found out about their orientation – some were arrested, some were beaten, some were accepted, at least in part.</p>
-      <p>In Malawi, my word was the only assurance I could give people that I wouldn’t reveal their identities. </p>
+      <p>In Malawi, my word was the only assurance I could give people that I wouldn’t reveal their identities.</p>
       <p>Malawi is not the most dangerous place in the world in terms of gay rights. People were comfortable enough that they would meet us and talk to us, and we could write these stories.</p>
       <p>But what about environments that are so harsh that the risks associated with talking to a reporter far outweigh the intangible benefits of a news article? As journalists, how do we report those stories?</p>
       <p>I don’t have a solution to those questions, and I don’t know if I will any time soon. But I know I want to try, because those are the most important stories to tell. ■</p>
     </div>
     <div class="large-7 columns story">
-      <h2 class="column-head">This is the headline for Blaine's column</h2>
+      <h2 class="column-head">Shooting in the shadows</h2>
       <div class="column-info"><span class="byline">By Blaine Ohigashi</span> • October 23, 2013</div>
       <img src="<?php echo SITE_URL; ?>img/day3/blaine.jpg" class="mugshot mugshot-small" />
-      <p class="dropcap">I was trying to put my anonymous subject at ease, assuring her that the portrait I took of her would not reveal her identity. </p>
+      <p class="dropcap">I was trying to put my anonymous subject at ease, assuring her that the portrait I took of her would not reveal her identity.</p>
       <p>I saw the photo merely as a tight, over-the-shoulder shot of her earring. But to her it was more. She was unable to put into words exactly why she thought she could be identified from the photo and all she could say was, “That’s me. Yes, that is me.”</p>
       <p>I was taken aback by this statement, and began to rethink all the portraits I had shot in the country until that point.</p>
       <p>I first saw this project as an opportunity to make images that could tell a story reaching beyond the bounds of the UCLA community. But after arriving in the country and embarking upon this photographic journey, I sometimes felt overwhelmed trying to juggle my subjects’ safety and the quality of storytelling in each photo.</p>
       <p>As a photojournalist, I strive to tell stories through meaningful images that resonate with viewers. Unlike my past work, however, for this series, the safety of my subjects was paramount, and I needed to protect their identities – not reveal them.</p>
       <p>The members of the LGBT community I was photographing were hiding in plain sight, and I was undertaking the seemingly paradoxical task of making photos that encapsulated who these people were without actually showing any physical features that could expose their identities. Doing that in an elegant way was a challenge.</p>
       <p>How could I be sure that I was successfully hiding their identities?</p>
-      <p>We let sources decide where and when we met to help maintain their anonymity and safety. I worked with the available lighting and locations in order to compose anonymous portraits – that  entailed everything from shooting through the slats of a wooden fence, to taking advantage of the shadows cast by a city streetlight at night.</p>
+      <p>We let sources decide where and when we met to help maintain their anonymity and safety. I worked with the available lighting and locations in order to compose anonymous portraits – that entailed everything from shooting through the slats of a wooden fence, to taking advantage of the shadows cast by a city streetlight at night.</p>
       <p>I made it work, and all of my subjects were okay with their photos. But now, in one of our last interviews of the trip, someone challenged me.</p>
-      <p>Photojournalism allows me to contribute positive change to the world by bringing informative images of stories, both big and small, to the masses. But in Malawi I was forced to think about who was best served by this contribution.  </p>
-      <p>Was I telling these stories to impact the way a country’s population regards the LGBT community? Was I telling these stories to help individuals who might not have the means to tell their own stories? Or was I telling these stories for myself? This was the first time I had thought about photojournalism so deeply, and I determined that I was doing a little bit of each. And it is for these reasons that I’ve allowed photojournalism to consume my life for the past five years. </p>
-      <p>I do think photojournalism has the power to captivate a generation, but that’s a rare occurrence. I’m not naïve enough to think I achieved this in Malawi. </p>
-      <p>But I did connect with people on a personal and emotional level. The individuals we talked to have hidden their sexuality from their friends and families for years, but they made the decision to share their stories with us, complete strangers. </p>
-      <p>Words on a page aren’t enough to do their stories justice. I can’t share the full identities of these Malawian men and women, but I hope my photos helped bring their stories to light.” ■</p>
+      <p>Photojournalism allows me to contribute positive change to the world by bringing informative images of stories, both big and small, to the masses. But in Malawi I was forced to think about who was best served by this contribution.</p>
+      <p>Was I telling these stories to impact the way a country’s population regards the LGBT community? Was I telling these stories to help individuals who might not have the means to tell their own stories? Or was I telling these stories for myself? This was the first time I had thought about photojournalism so deeply, and I determined that I was doing a little bit of each. And it is for these reasons that I’ve allowed photojournalism to consume my life for the past five years.</p>
+      <p>I do think photojournalism has the power to captivate a generation, but that’s a rare occurrence. I’m not naïve enough to think I achieved this in Malawi.</p>
+      <p>But I did connect with people on a personal and emotional level. The individuals we talked to have hidden their sexuality from their friends and families for years, but they made the decision to share their stories with us, complete strangers.</p>
+      <p>Words on a page aren’t enough to do their stories justice. I can’t share the full identities of these Malawian men and women, but I hope my photos helped bring their stories to light. ■</p>
     </div>
   </div>
 </article>
