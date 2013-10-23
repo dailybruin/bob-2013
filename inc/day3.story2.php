ba47e429b8c131cb0934d70ff06784e835edbbdd
Byron Lutz <byronlutz@gmail.com>
2013-10-23 03:22:21 -0700

    Replaced story 3.2

--- inc/day3.story2.php
+++ inc/day3.story2.php
@@ -25,11 +25,11 @@
         </figcaption>
       </figure>
       <p>“I just let him,” said the Malawian woman in her 20s, who asked to be called Mwayi for this story.</p>
-      <p>Activists helped Mwayi find a home after her family disowned her in February. In July, she had been living in her current house for two months. </p>
+      <p>Activists helped Mwayi find a home after her family disowned her in February. In July, she had been living in her current house for two months.</p>
       <p>In the struggle to gain rights for lesbian, gay, bisexual and transgender Malawians, some members of that community seem to be left out.</p>
-      <p>The reason that lesbian women in Malawi have been sidelined for so long is largely due to funding. Activists go after international HIV/AIDS funding to help the community, but that aid is mostly targeted toward men who have sex with men because they have a high risk of contraction.  </p>
+      <p>The reason that lesbian women in Malawi have been sidelined for so long is largely due to funding. Activists go after international HIV/AIDS funding to help the community, but that aid is mostly targeted toward men who have sex with men because they have a high risk of contraction.</p>
       <p>Anal sex is more likely to lead to sexually transmitted infections than vaginal sex because the lining of the anus is much thinner. Men who have sex with men are more likely than lesbian women or heterosexual couples to engage in anal sex, so they are at higher risk of contracting HIV.</p>
-      <p>“(Activists) take the pulse of the population and lawmakers and religious groups to understand what’s appropriate at a particular time,” said Ashley Currier, an assistant professor of women’s, gender and sexuality studies at the University of Cincinnati who researches LGBT organizing and homophobia around the world. </p>
+      <p>“(Activists) take the pulse of the population and lawmakers and religious groups to understand what’s appropriate at a particular time,” said Ashley Currier, an assistant professor of women’s, gender and sexuality studies at the University of Cincinnati who researches LGBT organizing and homophobia around the world.</p>
       <p>Activists in some countries, such as South Africa, have managed to tailor their public health approach partially to women. They use HIV/AIDS funding to target sexual violence against women, saying that attacks like what activists call “corrective rape,” the practice of raping lesbian women to make them straight, contribute to the spread of HIV. LGBT populations have constitutional protections in South Africa, in contrast to Malawi’s restrictive laws.</p>
       <figure>
         <img src="<?php echo SITE_URL ?>img/photo/04.Madaliso.JPG" />
@@ -38,22 +38,23 @@
           This woman, who asked to be called Madaliso for the story, is a lesbian woman in Malawi.
         </figcaption>
       </figure>
-      <p>UCLA has worked with the MAC AIDS Fund in South Africa to fund projects that help women who have been through these attacks. </p>
+      <p>UCLA has worked with the MAC AIDS Fund in South Africa to fund projects that help women who have been through these attacks.</p>
       <p>In Malawi, the resources for lesbians are scarce.</p>
-      <p>Fewer Malawian women are educated than men, so they have fewer employment opportunities. 2010 literacy rates were estimated at 68 percent for women older than 15, in contrast to 81 percent for men older than 15, according to the World Bank.  </p>
+      <p>Fewer Malawian women are educated than men, so they have fewer employment opportunities. In 2010, literacy rates were estimated at 68 percent for women older than 15, in contrast to 81 percent for men older than 15, according to the World Bank.</p>
       <p>This means women are more dependent on their families and more likely than men to live at home until marriage, which makes it harder for some lesbians to come out.</p>
-      <p>The original law banning homosexual acts applied only to men. In 2011, the Malawian Parliament extended it to women as well, though women face up to five years in prison and men face up to 14 years with hard labor. </p>
+      <p>The original law banning homosexual acts applied only to men. In 2011, the Malawian Parliament extended it to women as well, though women face up to five years in prison and men face up to 14 years with hard labor.</p>
       <p>Activists make the case that if it is illegal for men to have sex with men, it is difficult to get them the medical services they need.</p>
       <p>But the same argument can’t be made as strongly for women who have sex with women, so there are fewer opportunities to provide aid to women.</p>
       <p>At times, activists provide individual aid if a woman needs it, like in Mwayi’s case, but women’s rights organizations do not tailor their goals toward lesbians.</p>
       <p>Instead, those groups are largely focused on overall violence toward women, or toward empowering straight women to demand condom use and consistent HIV/AIDS testing from their male partners.</p>
-      <p>Oberlin College anthropology professor Crystal Biruk is writing a book about lesbians in Malawi, with Gift Trapence, the director of Malawi's primary gay rights organization, the Centre for the Development of People, also known as CEDEP.  This would be one of the first pieces of comprehensive research regarding lesbians in Malawi.</p>
+      <p>Oberlin College anthropology professor Crystal Biruk is writing a book about lesbians in Malawi, with Gift Trapence, the director of Malawi’s primary gay rights organization, the Centre for the Development of People, also known as CEDEP.  This would be one of the first pieces of comprehensive research regarding lesbians in Malawi.</p>
       <p>But Mwayi wants more, even though she doesn’t know how realistic her hopes are.</p>
       <p>She said lesbian women in the country should have access to scholarships for college tuition, as well as help getting jobs, so they have the independence to come out and live as lesbian women.</p>
       <p>CEDEP is also trying to create programs for lesbian women and prove that they exist in Malawi. The organization was founded in 2005 by a group of men who originally focused on gaps in HIV/AIDS treatment, so women were left out of the original strategic plan, said CEDEP administrator Dunker Kamba.</p>
       <p>Earlier this year, about 40 Malawian lesbians met with CEDEP to talk about their needs and rights.</p>
       <p>Mwayi was there, advocating for herself. ■</p>
-      </p>
+      <hr />
+      <p><em>Contributing reports by Blaine Ohigashi, Bruin senior staff.</em></p>
     </div>
     <div class="large-2 columns"> </div>
   </div>      
